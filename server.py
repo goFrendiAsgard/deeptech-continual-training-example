9dd9aba997f30deef75e3e3d9c9581674f9580ea
gofrendi <gofrendiasgard@gmail.com>
2020-01-29 07:43:51 +0700

    add accuracy graph

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
+import io
 import os
 import pickle
-from flask import Flask, request
 
-app = Flask(__name__)
+import pandas as pd
+import matplotlib.pyplot as plt
 
-@app.route("/", methods=["POST"])
+from flask import Flask, Response, request
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+
+@app.route("/", methods=["GET"])
+def main_index():
+    return app.send_static_file("index.html")
+
+
+@app.route("/predict", methods=["POST"])
 def main_predict():
     json_request = request.get_json()
 
@@ -25,9 +38,38 @@ def main_predict():
         petal_width = json_request["petal_width"]
 
     clf = pickle.load(open("./model/" + model_name + ".model", "rb"))
-    prediction = clf.predict([[float(sepal_length), float(sepal_width), float(petal_length), float(petal_width)]])
+    prediction = clf.predict([[
+        float(sepal_length), float(sepal_width),
+        float(petal_length), float(petal_width)
+    ]])
 
     return {"result": prediction[0]}
+
+
+@app.route("/accuracy-history")
+def main_accuracy_history():
+    df = pd.read_csv(
+        "./accuracy/log.csv", header=None,
+        names=["time", "accuracy"]
+    )
+    df = df.tail(10)
+
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(df["time"], df["accuracy"], "-")
+    ax.tick_params(axis='x', labelrotation=90)
+    ax.set_title("Accuracy")
+    ax.set_ylim([0, 1])
+    ax.grid(True)
+
+    fig.tight_layout()
+
+    canvas = FigureCanvas(fig)
+    png_output = io.BytesIO()
+    canvas.print_png(png_output)
+    response = Response(png_output.getvalue(), mimetype="image/png")
+    return response
+
 
 if __name__ == "__main__":
     debug_mode = os.environ["DEBUG_MODE"] == "1"
